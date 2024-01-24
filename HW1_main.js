65cd69453015058062605d893ce0488587e4a706
sanjitabh <68780844+sanjitabh@users.noreply.github.com>
2024-01-23 20:54:57 -0500

    Add files via upload

---
+++ HW1_main.js
@@ -0,0 +1,34 @@
+
+let myButton = document.getElementById("myButton");
+let myImage = document.getElementById("myImage");
+
+myButton.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "tulips.jpg") {
+    myImage.setAttribute("src", "nyc.jpg");
+  } else {
+    myImage.setAttribute("src", "tulips.jpg");
+  }
+};
+
+
+let myButton2 = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Welcome, ${myName}`;
+  myHeading.style.color = "#556B2F"; // Set text color
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Welcome, ${storedName}`;
+  myHeading.style.color = "#556B2F"; // Set text color
+}
+myButton2.onclick = () => {
+  setUserName();
+};
